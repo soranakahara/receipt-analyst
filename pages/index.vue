e9e4b1e990255f4d303073122091d1b4caad6326
Sora Nakahara <skyfish1003@gmail.com>
2022-07-08 18:16:01 +0900

    add mainChart

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="row">
-    <!-- <h1>入力フォーム</h1>
-    <form> -->
-        <!-- date <input v-model="receipt.date" placeholder="date"><br> -->
-        <!-- total <input v-model="total_price" placeholder="total"><br> -->
-        <!-- ボタンの処理をvueで書く。formの外に書く！ -->
-    <!-- </form>
-    <div v-for="detail in receipt.details" v-bind:key="detail.id">
-      <detail-form v-bind="detail"></detail-form>
+    <!-- Main Chart -->
+    <div class="col-lg-12" :class="{ 'text-right': isRTL }">
+      <card type="chart">
+        <template slot="header">
+          <div class="row">
+            <div class="col-sm-6" :class="isRTL ? 'text-right' : 'text-left'">
+              <h5 class="card-category">Total shipments</h5>
+              <h2 class="card-title">Performance</h2>
+            </div>
+            <div class="col-sm-6 d-flex d-sm-block">
+              <div
+                class="btn-group btn-group-toggle"
+                :class="isRTL ? 'float-left' : 'float-right'"
+                data-toggle="buttons"
+              >
+                <label
+                  v-for="(option, index) in mainChartCategories"
+                  :key="option.name"
+                  class="btn btn-sm btn-primary btn-simple"
+                  :class="{ active: blueBarChart.activeIndex === index }"
+                  :id="index"
+                >
+                  <input
+                    type="radio"
+                    @click="initMainChart(index)"
+                    name="options"
+                    autocomplete="off"
+                    :checked="blueBarChart.activeIndex === index"
+                  />
+                  <span class="d-none d-sm-block">{{ option.name }}</span>
+                  <span class="d-block d-sm-none">
+                    <div>{{ option.shortname }}</div>
+                  </span>
+                </label>
+              </div>
+            </div>
+          </div>
+        </template>
+        <div class="chart-area">
+          <bar-chart
+            style="height: 100%"
+            ref="mainChart"
+            :chart-data="blueBarChart.chartData"
+            :gradient-stops="blueBarChart.gradientStops"
+            :extra-options="blueBarChart.extraOptions"
+          >
+          </bar-chart>
+        </div>
+      </card>
     </div>
-    <button v-on:click="addFrom">フォームを追加</button>
-    <button v-on:click="postData">データベースに保存</button> -->
-
-
     <!-- Big Chart -->
     <div class="col-12">
       <card type="chart">
@@ -213,6 +250,20 @@ let bigChartDatasetOptions = {
   pointRadius: 4,
 }
 
+let mainChartData = [
+  [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
+  [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
+  [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
+]
+let mainChartLabels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+let mainChartDatasetOptions = {
+  fill: true,
+  borderColor: config.colors.info,
+  borderWidth: 2,
+  borderDash: [],
+  borderDashOffset: 0.0,
+}
+
 export default {
   name: 'dashboard',
   components: {
@@ -343,23 +394,18 @@ export default {
         gradientStops: [1, 0.4, 0]
       },
       blueBarChart: {
-        extraOptions: chartConfigs.barChartOptions,
+        activeIndex: 0,
         chartData: {
-          labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-          datasets: [
-            {
-              label: 'Countries',
-              fill: true,
-              borderColor: config.colors.info,
-              borderWidth: 2,
-              borderDash: [],
-              borderDashOffset: 0.0,
-              data: [53, 20, 10, 80, 100, 45]
-            }
-          ]
+          datasets: [{
+              ...mainChartDatasetOptions,
+              // label: 'FoodCosts',
+              data: mainChartData[0]
+            }],
+          labels: mainChartLabels,
         },
-        gradientColors: config.colors.primaryGradient,
-        gradientStops: [1, 0.4, 0]
+        extraOptions: chartConfigs.barChartOptions,
+        gradientStops: [1, 0.4, 0],
+        categories: [],
       }
     };
   },
@@ -390,6 +436,14 @@ export default {
         name: 'Purchases',
         icon: 'tim-icons icon-gift-2'
       }, { name: 'Sessions', icon: 'tim-icons icon-tap-02' }];
+    },
+    mainChartCategories () {
+      return [
+        { name: 'Daily', shortname: 'Daily' },
+        { name: 'Weekly', shortname: 'Weekly'},
+        { name: 'Monthly', shortname: 'Monthly' },
+        { name: 'Annual', shortname: 'Annual' }
+      ];
     }
   },
   methods: {
@@ -426,10 +480,23 @@ export default {
       this.$refs.bigChart.updateGradients(chartData);
       this.bigLineChart.chartData = chartData;
       this.bigLineChart.activeIndex = index;
+    },
+    initMainChart (index) {
+      let cData = {
+        datasets: [{
+          ...mainChartDatasetOptions,
+          data: mainChartData[index]
+        }],
+        labels: mainChartLabels
+      };
+      this.$refs.mainChart.updateGradients(cData); // ref="mainChart"で定義したコンポーネントを触れる
+      this.blueBarChart.chartData = cData;
+      this.blueBarChart.activeIndex = index;
     }
   },
   mounted () {
     this.initBigChart(0);
+    this.initMainChart(0);
   }
 }
 </script>
